12a4a1e196e934f6aa60a90ac1babd637bffba6c
Vishwajeet <v.khaple030203@gmail.com>
2024-12-16 18:03:36 +0530

    Socket Implemented

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static("/public"));
+//socket.io
+io.on('connection', (socket) => {
+    socket.on('user-message', message => {
+        console.log("A new User Message ", message);
+    })
+})
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req, res) => {
+    return res.sendFile('./public/index.html');
+});
 
 server.listen(2003, () => console.log('server started at port: 2003'))
 
